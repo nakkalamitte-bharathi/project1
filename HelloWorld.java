8c4c449743f3c4827de2fc173a4df3d105d74f3b
nakkalamitte-bharathi <97883471+nakkalamitte-bharathi@users.noreply.github.com>
2022-06-11 09:01:49 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld
 {
 	public static void main(String s[]){
-		System.out.println("Welcome to Java World After Changes");
+		System.out.println("Welcome to Java World After 2nd Changes");
 	}
 }
